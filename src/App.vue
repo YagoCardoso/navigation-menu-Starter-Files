18dc3e67abdc8a5b5564e87fc1d90af2c5af8ef3
Yago Maksuell Lacerda Cardoso <yago.cardoso@linx.com.br>
2022-11-03 14:13:29 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app">
+    <Navigaton/>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigaton from "./components/Navigation.vue"
+export default {
+  name: "App",
+  components:{
+    Navigaton
+
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Raleway", sans-serif;
+  font-weight: 400;
+}
+
+.app {
+  min-height: 100vh;
+  position: relative;
+  background-color: #f1f1f1;
+}
+.container {
+  padding: 0 20px;
+  max-width: 1140px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,50 @@
+<template>
+  <header :class="{ 'scrolled-nav': scrollPosition }">
+    <nav>
+      <div class="branding">
+        <img src="@/assets/logo.png" alt="" />
+      </div>
+      <ul v-show="!mobile" class="navigation">
+        <li>
+          <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+        </li>
+        <li>
+          <router-link class="link" :to="{ name: 'Home' }">Home2</router-link>
+        </li>
+        <li>
+          <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+        </li>
+      </ul>
+      <div class="icon">
+        <i
+          @click="toggleMobileNav"
+          v-show="mobile"
+          class="far fa-bars"
+          :class="{ 'icon-active': mobileNav }"
+        ></i>
+      </div>
+      <transition name="mobile-nav">
+        <ul v-show="!mobileNav" class="dropdown-nav">
+          <li>
+            <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+          </li>
+          <li>
+            <router-link class="link" :to="{ name: 'Home' }">Home2</router-link>
+          </li>
+          <li>
+            <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
+          </li>
+        </ul>
+      </transition>
+    </nav>
+  </header>
+  <div></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
